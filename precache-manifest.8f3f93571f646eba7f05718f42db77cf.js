cd2fa88d403a31d9553a3b37cddcbb1315a2fd12
Adryian <adryian.siarheyeu@gmail.com>
2019-06-04 16:27:14 +0300

    Updates

---
+++ precache-manifest.8f3f93571f646eba7f05718f42db77cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d70f980563b859bfef3ce5a933c9c1d",
+    "url": "/iTechArt/index.html"
+  },
+  {
+    "revision": "94b9bec4342c935285d9",
+    "url": "/iTechArt/static/css/main.6ca7593d.chunk.css"
+  },
+  {
+    "revision": "c213f09d8ff3080788cf",
+    "url": "/iTechArt/static/js/2.dcd5ff40.chunk.js"
+  },
+  {
+    "revision": "94b9bec4342c935285d9",
+    "url": "/iTechArt/static/js/main.5f927e78.chunk.js"
+  },
+  {
+    "revision": "abd54cf09a04fdaf990b",
+    "url": "/iTechArt/static/js/runtime~main.8ad7a516.js"
+  }
+]);
