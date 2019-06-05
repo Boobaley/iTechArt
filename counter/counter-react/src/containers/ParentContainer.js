c2239d4760b956fb77fed55da43dd5061d838484
Adryian <adryian.siarheyeu@gmail.com>
2019-06-05 22:55:40 +0300

    First implementation of controling quantity of counters

--- counter/counter-react/src/containers/ParentContainer.js
+++ counter/counter-react/src/containers/ParentContainer.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import CounterContainer from './CounterContainer';
-import ParentCounter from '../views/ParentCounter/ParentCounter';
+import CounterOfCounters from '../views/CounterOfCounters/CounterOfCounters';
 
 class ParentContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-           arrOfCounters: [<CounterContainer />]
+            counters: 1
         }
 
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleDelette = this.handleDelette.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
 
     handleAdd() {
-        console.log(this.state.arrOfCounters);
+        this.setState({counters: this.state.counters + 1});
+    }
+
+    handleDelette() {
+        if (this.state.counters > 1) {
+            this.setState({counters: this.state.counters - 1})
+        } else {
+            this.setState({counters: 1})
+        } 
+    }
+
+    handleReset() {
+        this.setState({counters: 1})
     }
 
     render() {
+        const counter = <CounterContainer/>;
+        let counters = [];
+
+        for (let i = 0; i < this.state.counters; i++) {
+            counters.push(counter);
+        }
+       
         return (
-           <ParentCounter 
-               arr={this.state.arrOfCounters}
-               adding={this.handleAdd}
-           />
+            <div>
+                <CounterOfCounters 
+                    add={this.handleAdd}
+                    delette={this.handleDelette}
+                    reset={this.handleReset}
+                />
+                {counters.map((item, id) => 
+                    <div key={id}>
+                        {item}
+                    </div>
+                )}
+            </div>
+           
         );
     }
 }
