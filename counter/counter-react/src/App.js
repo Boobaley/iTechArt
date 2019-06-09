4c61cfc19278f7b8c1a9516041d9cdcd3d0e0c66
Adryian <adryian.siarheyeu@gmail.com>
2019-06-10 00:02:58 +0300

    React-router added; --Pre-relise version--

--- counter/counter-react/src/App.js
+++ counter/counter-react/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import ParentContainer from './containers/ParentContainer';
+import Tabs from './views/Tabs/Tabs';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import About from './views/About/About';
+import Error from './views/Error/Error';
+
 
 const App = () => {
   return (
-    <div>
-      <ParentContainer />
-    </div>
+    <BrowserRouter>
+        <div> 
+            <Route path='/' component={Tabs}/>
+            <Switch>
+                <Route path='/about' component={About}/>
+                <Route path='/counters' component={ParentContainer}/>
+                <Route component={Error}/> 
+            </Switch>
+        </div>
+    </BrowserRouter>
   )
 }
 
