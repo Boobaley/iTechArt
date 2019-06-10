53fb260eb994c8948374a98dc6ba0de6385a3b83
Adryian <adryian.siarheyeu@gmail.com>
2019-06-10 12:51:35 +0300

    fixes with gh-pages routing

--- counter/counter-react/src/App.js
+++ counter/counter-react/src/App.js
@@ -1,50 +1,12 @@
 import React from 'react';
 import './App.css';
-import ParentContainer from './containers/ParentContainer';
-import Tabs from './views/Tabs/Tabs';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import About from './views/About/About';
-import Error from './views/Error/Error';
+import Routes from './views/Router/Routes';
+
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-        <div> 
-            <Switch>
-                <Route 
-                    exact
-                    path='/' 
-                    render={() =>
-                        <div>
-                            <Tabs current={null}/>
-                        </div>
-                    }
-                />
-                <Route 
-                    path='/about' 
-                    render={() =>
-                        <div>
-                            <Tabs current={0}/>
-                            <About/>
-                        </div>
-                    }
-                />
-                <Route 
-                    path='/counters' 
-                    render={() =>
-                        <div>
-                            <Tabs current={1}/>
-                            <ParentContainer/>
-                        </div>
-                    }
-                />
-                <Route render={() =>
-                    <Error/>
-                }/> 
-            </Switch>
-        </div>
-    </BrowserRouter>
+    <Routes/>
   )
 }
 
