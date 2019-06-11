a3533d274c397aff961c6071fd2d418bf1e97774
Adryian <adryian.siarheyeu@gmail.com>
2019-06-11 14:27:06 +0300

    inline style relocate to new file; code-style fix

--- counter/counter-react/src/views/Tabs/Tabs.js
+++ counter/counter-react/src/views/Tabs/Tabs.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { tabStyles } from './styles';
 import { Link } from 'react-router-dom';
 const useStyles = makeStyles({
     root: {
@@ -17,20 +18,20 @@ const useStyles = makeStyles({
     function handleChange(event, newValue) {
       setValue(newValue);
     }
-
-  return (
-    <Paper className={classes.root}>
-      <Tabs 
-        value={value} 
-        indicatorColor="primary" 
-        textColor="primary" 
-        onChange={handleChange}
-        centered
-        style={{marginBottom:20}}
-      >
-        <Tab label="About Us" component={Link} to="/about"/>
-        <Tab label="Counters" component={Link} to="/counters"/>
-      </Tabs>
-    </Paper>
-  );
+    
+    return (
+      <Paper className={classes.root}>
+        <Tabs 
+          value={value} 
+          indicatorColor="primary" 
+          textColor="primary" 
+          onChange={handleChange}
+          centered
+          style={tabStyles}
+        >
+          <Tab label="About Us" component={Link} to="/about"/>
+          <Tab label="Counters" component={Link} to="/counters"/>
+        </Tabs>
+      </Paper>
+    );
 }
---
+++ counter/counter-react/src/views/Tabs/styles.js
@@ -0,0 +1,5 @@
+const tabStyles = {
+    marginBottom: 20
+}
+
+export { tabStyles };
